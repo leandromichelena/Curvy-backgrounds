d092587ba0f067f3da87a0916eeb138b41c0ec83
Leandro Michelena <leandromichelena@gmail.com>
2024-03-20 13:23:38 -0400

    Separated CSS and script sections into different files and linked in HTML file.

---
+++ script.js
@@ -0,0 +1,9 @@
+// SVG Morph animation library Kute.js
+const tween = KUTE.fromTo(
+    '#blob1', 
+    { path: '#blob1' }, 
+    { path: '#blob2' }, 
+    { repeat: 999, duration: 2000, yoyo: true }
+);
+
+tween.start();
